0ae012b7393ea6624c5bc78d74f5bb71f4b6547e
rchin00 <rachaelmchin@gmail.com>
2022-02-23 22:18:21 -0500

    bankteller

---
+++ Project2/src/src/BankTeller.java
@@ -0,0 +1,11 @@
+package src;
+
+public class BankTeller {
+
+    private void runCommand(String userCommand, String [] userInput) {
+    }
+
+    private boolean isValidLocation(int location){
+
+    }
+}
