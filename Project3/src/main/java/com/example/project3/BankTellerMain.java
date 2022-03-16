654420ce71040746c78145141e293a5d6fe7e280
Sebastian <40216200+SebastianACarrasco@users.noreply.github.com>
2022-03-16 16:36:12 -0400

    finished GUI

---
+++ Project3/src/main/java/com/example/project3/BankTellerMain.java
@@ -0,0 +1,41 @@
+package com.example.project3;
+/**
+ * This is the main class for running the GUI application as it contains the
+ * main method. This sets up the stage, so the dimensions and title are created
+ * here.
+ *
+ * @author Sebastian Carrasco, Rachael Chin
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BankTellerMain extends Application {
+
+    /**
+     * The start method sets up the stage and scene with dimensions and title
+     * and lastly displays it.
+     * @param stage
+     * @throws IOException
+     */
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(BankTellerMain.class.getResource("BankTellerView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 650);
+        stage.setTitle("BankTeller");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Main method for running the application as it only contains launch().
+     * @param args
+     */
+    public static void main(String[] args) {
+        launch();
+    }
+}
