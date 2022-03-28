021ba2fe1a527e103b1c0b19b70eb5dda2520d09
Sebastian <40216200+SebastianACarrasco@users.noreply.github.com>
2022-03-27 22:10:34 -0400

    Added P4 files

--- Project4/src/main/java/com/example/project4/mainViewApplication.java
+++ Project4/src/main/java/com/example/project4/mainViewApplication.java
@@ -12,7 +12,7 @@ public class mainViewApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(mainViewApplication.class.getResource("mainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Welcome to RU Cafe!");
         stage.setScene(scene);
         stage.show();
     }
