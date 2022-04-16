90a46ccf1586623135bca0a8bf14b3ac2fbf8f10
Sebastian <40216200+SebastianACarrasco@users.noreply.github.com>
2022-04-16 15:44:49 -0400

    worked on store order and basket

--- Project5/app/src/main/java/com/example/project5/MainActivity.java
+++ Project5/app/src/main/java/com/example/project5/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.project5;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Opens donut ordering when clicked
+     */
+    public void donutsButton(View view) {
+        //Intent intent = new Intent(this, DonutsActivity.class);
+        //startActivity(intent);
+    }
+    /**
+     * Opens coffee ordering when clicked
+     */
+    public void coffeeButton(View view) {
+        //Intent intent = new Intent(this, CoffeeActivity.class);
+        //startActivity(intent);
+    }
+    /**
+     * Opens order basket when clicked
+     */
+    public void orderBasket(View view) {
+        Intent intent = new Intent(this, orderBasketActivity.class);
+        startActivity(intent);
+    }
+    /**
+     * Opens store orders when clicked
+     */
+    public void storeOrders(View view) {
+        //Intent intent = new Intent(this, StoreordersActivity.class);
+        //startActivity(intent);
     }
 }
---
+++ Project5/app/src/main/java/com/example/project5/orderBasketActivity.java
@@ -0,0 +1,12 @@
+package com.example.project5;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class orderBasketActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orderbasket);
+    }
+}
