5d6967c7fd190866d5c3e3d397bf2146550aab2c
Sebastian <40216200+SebastianACarrasco@users.noreply.github.com>
2022-04-16 20:28:01 -0400

    worked on coffee

--- Project5/.idea/sonarlint/issuestore/index.pb
+++ Project5/.idea/sonarlint/issuestore/index.pb
@@ -13,11 +13,7 @@ a
 1app/src/main/java/com/example/project5/Order.java,d/c/dccbb2ac09fc3b45dcff7dd0af14899f6808c452
 g
 7app/src/main/java/com/example/project5/StoreOrders.java,8/f/8f412a3cc70264d365c8200fd71631f4c8d9b160
-b
-2app/src/main/java/com/example/project5/Coffee.java,3/9/3922790ba39f49525a884790259d34b5cafda5f9
 k
 ;app/src/test/java/com/example/project5/ExampleUnitTest.java,6/a/6a8c3c49e053eaeda7bb1600b3d769e85211597a
-P
- app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
 <
 build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
--- Project5/app/src/main/java/com/example/project5/MainActivity.java
+++ Project5/app/src/main/java/com/example/project5/MainActivity.java
@@ -1,45 +1,68 @@
 package com.example.project5;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ImageButton;
 
+import java.io.Serializable;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Home screen for the app that allows the user to navigate to the other activities.
+ *
+ * @author Sebastian Carrasco, Rachael Chin
+ */
+public class MainActivity extends AppCompatActivity implements Serializable {
+    private Order order = new Order();
 
+    /**
+     * Creates the activity on the screen
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    /**
-     * Opens donut ordering when clicked
-     */
-    public void donutsButton(View view) {
-        //Intent intent = new Intent(this, DonutsActivity.class);
-        //startActivity(intent);
-    }
-    /**
-     * Opens coffee ordering when clicked
-     */
-    public void coffeeButton(View view) {
-        //Intent intent = new Intent(this, CoffeeActivity.class);
-        //startActivity(intent);
-    }
-    /**
-     * Opens order basket when clicked
-     */
-    public void orderBasket(View view) {
-        Intent intent = new Intent(this, OrderBasketActivity.class);
-        startActivity(intent);
-    }
-    /**
-     * Opens store orders when clicked
-     */
-    public void storeOrders(View view) {
-        Intent intent = new Intent(this, StoreOrderActivity.class);
-        startActivity(intent);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setTitle("Main Menu");
+
+        //donuts
+        ImageButton donuts = findViewById(R.id.homeDonuts);
+
+        //coffee
+        ImageButton coffee = findViewById(R.id.homeCoffee);
+        coffee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CoffeeActivity.class);
+                //intent.putExtra("order", (Serializable) order);
+                startActivity(intent);
+            }
+        });
+
+        //basket
+        ImageButton orderBasket = findViewById(R.id.imageButton4);
+        orderBasket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OrderBasketActivity.class);
+                //intent.putExtra("order", (Serializable) order);
+                startActivity(intent);
+            }
+        });
+
+        //store order
+        ImageButton storeOrder = findViewById(R.id.imageButton5);
+        storeOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, StoreOrderActivity.class);
+                //intent.putExtra("order", (Serializable) order);
+                startActivity(intent);
+            }
+        });
     }
 }
