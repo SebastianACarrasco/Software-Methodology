c8a3a7b8259e5f4e41a2f16c40d0c5882773323a
rchin00 <rachaelmchin@gmail.com>
2022-04-25 17:33:53 -0400

    donuts

--- Project5/app/src/main/java/com/example/project5/StoreOrderActivity.java
+++ Project5/app/src/main/java/com/example/project5/StoreOrderActivity.java
@@ -1,12 +1,56 @@
 package com.example.project5;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class StoreOrderActivity extends AppCompatActivity{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storeorders);
+
+        Button remove = findViewById(R.id.removeOrder);
+        ListView listView = findViewById(R.id.listViewOrder);
+
+        //Intent intent = new Intent(OrderBasketActivity.class, StoreOrderActivity.this);
+
+        //alert dialog for removing items from the order
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(StoreOrderActivity.this);
+                builder.setMessage("Do you want to remove this order?");
+                builder.setTitle("Alert!");
+
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //orderList.remove(position);
+                        //arrayAdapter.notifyDataSetChanged();
+                    }
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
     }
 }
